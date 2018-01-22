78c35016964709273c364bc8aabd23ea1d645e4f
Deniss <den.b@itservicesgroup.co.uk>
2018-01-22 21:13:59 +0000

    Add Rspec config files
    Config faker, rspec and factory_bot_rails

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'faker'
+require 'factory_bot_rails'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.include FactoryBot::Syntax::Methods
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+end
