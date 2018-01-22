6e0faf8e3db0cade04cba6f30f5c37a092a12538
Deniss <den.b@itservicesgroup.co.uk>
2018-01-22 22:08:57 +0000

    Add Spec when booking conflicts with passed end date

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe BookingsController, type: :controller do
   end
 
    describe '#create' do
+
      context 'when booking conflicts with passed start date' do
        before do
          @room = create(:room)
@@ -21,6 +22,26 @@ RSpec.describe BookingsController, type: :controller do
        it 'should return json message' do
 
          post :create, {room_id: @room.id, start: @options[:start].to_s}, format: :json
+         message = {"message" => "Booking conflicts with an existing booking"}
+         output = json_parser(response.body)
+         expect(output).to eq(message)
+       end
+     end
+
+     context 'when booking conflicts with passed end date' do
+       before do
+         @room = create(:room)
+         @options = {start: Date.today, end: Date.today + 3.days}
+         @room.bookings.create(@options)
+       end
+       it 'should return unprocessable_entity' do
+
+         post :create, {room_id: @room.id, end: @options[:end].to_s}, format: :json
+         expect(response).to have_http_status(422)
+       end
+       it 'should return json message' do
+
+         post :create, {room_id: @room.id, end: @options[:end].to_s}, format: :json
          message = {"message" => "Booking conflicts with an existing booking"}
          output = json_parser(response.body)
          expect(output).to eq(message)
