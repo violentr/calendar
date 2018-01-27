c3e65b3b936e6adb568bea4a17fab3738083d123
Deniss <den.b@itservicesgroup.co.uk>
2018-01-27 15:49:59 +0000

    Add Spec, booking when passed start_date <= passed end_date

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe BookingsController, type: :controller do
          @room = create(:room)
          @options = {start: Date.today, end: Date.today + 3.days}
          3.times {@room.bookings.create(@options) }
-         @start_date = (Date.today + 1.week).to_s 
+         @start_date = (Date.today + 1.week).to_s
          @end_date = (Date.today + 5.days).to_s
        end
 
@@ -65,6 +65,29 @@ RSpec.describe BookingsController, type: :controller do
 
          post :create, {room_id: @room.id, start: @start_date, end: @end_date}, format: :json
          message = {"message" => "Booking created."}
+         output = json_parser(response.body)
+         expect(output).to eq(message)
+       end
+     end
+     #TO DO
+     context 'when booking passed start date <= passed end date' do
+       before do
+         @room = create(:room)
+         @options = {start: Date.today + 5.days, end: Date.today + 5.days}
+         3.times {@room.bookings.create(@options) }
+         @start_date = Date.today.to_s
+         @end_date = (Date.today + 6.days).to_s
+       end
+
+       it 'should return json http_error unprocessable_entity' do
+         post :create, {room_id: @room.id, start: @start_date, end: @end_date}, format: :json
+         expect(response).to have_http_status(:unprocessable_entity)
+       end
+
+       it 'should return json message "Booking conflicts with an existing boqoking"' do
+
+         post :create, {room_id: @room.id, start: @start_date, end: @end_date}, format: :json
+         message = {"message" => "Booking conflicts with an existing booking"}
          output = json_parser(response.body)
          expect(output).to eq(message)
        end
